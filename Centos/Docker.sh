2ce4993535111f3326dd26ee6cbce0a045add53d
StartXG <1997gncGNC>
2022-02-11 14:51:13 +0800

    🚀：增加docker

---
+++ Centos/Docker.sh
@@ -0,0 +1,18 @@
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io
+systemctl restart docker
+
+echo '{"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn/"]}' >> /etc/docker/daemon.json
+
+systemctl restart docker
